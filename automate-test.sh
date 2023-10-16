a1c363287943104b8a449b3243e04b6a9bad01bb
craig91 <craigdejean1@gmail.com>
2023-10-16 18:59:40 -0400

    Added comment to conditional

--- automate-test.sh
+++ automate-test.sh
@@ -17,14 +17,14 @@
 
 
 
-# Program array, add any software you want to instal to this array, make sure to verify the official name from your package manager.
+# Program array, add any software you want to instal to this array, make sure to verify the official name from your package manager.-
 Binaries=("vim" "nodejs" "okular" "simplescreenrecorder" "terminator" "gparted" "git" "curl" "gpg" "wget" "snapd")
 
 
 system_setup_debian() {
 	sudo apt update
 	for prog in "${Binaries[@]}"; do
-		if ! dpkg -l | grep -q $prog; then
+		if ! dpkg -l | grep -q $prog; then # --> Checks if the binary is in the package list (run command 'man dpkg' and look for the -l flag)
 			sudo apt install -y $prog
 			if [ $? -eq 0 ]; then
 				echo "Installed $prog successfully"
