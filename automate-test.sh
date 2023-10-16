5095043c5dd724998cae2ffc6133e59c4cce9aa3
craig91 <craigdejean1@gmail.com>
2023-10-16 14:57:06 -0400

    Updated system setup script

--- automate-test.sh
+++ automate-test.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-apt install git
-mkdir Code && cd $_
-git clone https://github.com/craig91/Linux-Scripts.git
+Binaries=("vim", "nodejs", "okular", "simplescreenrecorder", "terminator", "gparted", "git", "curl")
+
+
+system_setup_debian() {
+	for prog in "${Binaries[@]}"; do
+		if ! dpkg | grep -q $prog; then
+			sudo apt update
+			sudo apt install -y $prog
+			if [ $? -eq 0 ] then
+				echo "Installed $prog successfully"
+			else
+				echo "Failed to install $prog"
+			fi
+		else
+			echo "$prog is already installed"
+		fi
+	done
+}
+
+if ["EUID" -ne 0]; then
+	echo "Run this script with root ( ex: sudo ./<script>)."
+	exit 1
+fi
+
+system_setup_debian
--- test3.sh
+++ test3.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
+if ! command -v dialog > /dev/null; then
+	sudo apt-get install dialog
+fi
+
+dialog --menu "Please select an option:" 12 40 4 \
+	1 "Option A" \
+	2 "Option B" \
+	3 "Option C" \
+	4 "Quit" 2> /tmp/choice
+
+choice=$(cat /tmp/choice)
+
+rm -f /tmp/choice
+
+case "$choice" in
+    1)
+        echo "You selected Option A."
+        # Add code to execute for Option A
+        ;;
+    2)
+        echo "You selected Option B."
+        # Add code to execute for Option B
+        ;;
+    3)
+        echo "You selected Option C."
+        # Add code to execute for Option C
+        ;;
+    4)
+        echo "Goodbye!"
+        exit 0
+        ;;
+    *)
+        echo "Invalid choice. Please select a valid option."
+        ;;
+esac
 
