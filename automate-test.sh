150dc0e8ce809c72010a815f0d3d56928c1f7564
craig91 <craigdejean1@gmail.com>
2023-10-16 21:47:45 -0400

    moved functions up as the program doesn't know where the function logic is

--- automate-test.sh
+++ automate-test.sh
@@ -21,6 +21,42 @@ if ! command -v dialog > /dev/null; then
 	sudo apt-get install dialog
 fi
 
+system_setup_debian() {
+	sudo apt update
+	for prog in "${Binaries[@]}"; do
+		if ! dpkg -l | grep -q $prog; then # --> Checks if the binary is in the package list (run command 'man dpkg' and look for the -l flag)
+			sudo apt install -y $prog
+			if [ $? -eq 0 ]; then
+				echo "Installed $prog successfully"
+			else
+				echo "Failed to install $prog"
+			fi
+		else
+			echo "$prog is already installed"
+		fi
+	done
+}
+
+if ["$EUID" -ne 0]; then
+	echo "Run this script with root ( ex: sudo ./<script>)."
+	exit 1
+fi
+
+
+
+chrome_install() {
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	sudo dpkg -i google-chrome-stable_current_amd64.deb
+}
+
+
+
+visualStudio_code_install() {
+	sudo snap install --classic code
+}
+
+
+
 dialog --menu "Please select an option:" 12 40 4 \
 	1 "Debian" \
 	2 "RedHat" \
@@ -58,37 +94,5 @@ esac
 
 
 
-system_setup_debian() {
-	sudo apt update
-	for prog in "${Binaries[@]}"; do
-		if ! dpkg -l | grep -q $prog; then # --> Checks if the binary is in the package list (run command 'man dpkg' and look for the -l flag)
-			sudo apt install -y $prog
-			if [ $? -eq 0 ]; then
-				echo "Installed $prog successfully"
-			else
-				echo "Failed to install $prog"
-			fi
-		else
-			echo "$prog is already installed"
-		fi
-	done
-}
 
-if ["$EUID" -ne 0]; then
-	echo "Run this script with root ( ex: sudo ./<script>)."
-	exit 1
-fi
-
-
-
-chrome_install() {
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	sudo dpkg -i google-chrome-stable_current_amd64.deb
-}
-
-
-
-visualStudio_code_install() {
-	sudo snap install --classic code
-}
 
