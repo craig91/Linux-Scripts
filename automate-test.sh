53a1c7022af3a8ea78894c8f877a06290f626015
craig91 <craigdejean1@gmail.com>
2023-12-06 20:53:18 -0500

    Added Arch Linux install functions to main script

--- automate-test.sh
+++ automate-test.sh
@@ -15,6 +15,8 @@
 
 # Program array, add any software you want to instal to this array, make sure to verify the official name from your package manager.-
 Binaries=("vim" "nodejs" "okular" "simplescreenrecorder" "terminator" "gparted" "git" "curl" "gpg" "wget" "snapd" "gcc" "make" "mysql-server")
+Arch_Binaries=("vim" "nodejs" "terminator" "gparted" "git" "curl" "gpg" "gcc" "make" "go" "discord" "neofetch")
+
 
 system_setup_debian() {
 	sudo apt update
@@ -32,11 +34,54 @@ system_setup_debian() {
 	done
 }
 
+system_setup_Arch() {
+    sudo pacman -Syu
+    for prog in "${Arch_Binaries[@]}"; do
+        if ! pacman -Q | grep -q $prog; then
+            sudo pacman -S $prog
+            if [$? -eq 0]; then
+                echo "Installed $prog successfully"
+            else
+                echo "Failed to install $prog"
+            fi
+        else
+            echo "$prog: ---> is already installed"
+        fi
+    done
+}
+
+
+
+
 if ["$EUID" -ne 0]; then
 	echo "Run this script with root ( ex: sudo ./<script>)."
 	exit 1
 fi
 
+
+install_japanese() {
+    cd /home/$USER/Downloads
+    git clone https://aur.archlinux.org/ttf-koruri.git
+    cd ttf-koruri
+    makepkg -si
+}
+
+
+
+install_Fira_Code() {
+    sudo pacman -S unzip
+    if [ ! -d /home/$USER/.local/share/fonts/nerd-fonts ]; then
+        mkdir -p /home/$USER/.local/share/fonts/nerd-fonts
+    fi
+    echo "[+] Downloading Fonts [+]"
+    wget -P ~/.local/share/fonts/nerd-fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
+    cd /home/$USER/.local/share/fonts/nerd-fonts
+    pwd
+    unzip FiraCode.zip
+    fc-cache -vf
+    echo "[+] Confirm if FiraCode is on this list of fonts in your system [+]"
+    fc-list | grep FiraCode
+}
 
 
 chrome_install() {
@@ -51,12 +96,9 @@ chrome_install() {
 	fi
 }
 
-
-
 visualStudio_code_install() {
 	sudo snap install --classic code
 }
-
 
 rust_install() {
 	
@@ -98,7 +140,10 @@ case "$choice" in
 		;;
 	3)
 		echo "You chose Arch based linux, now installing software"
-		echo "Arch linux stuff now installing"
+		system_setup_Arch
+		install_Fira_Code
+		install_japanese
+		go version
 		;;
 	4)
 		echo "Bye"
