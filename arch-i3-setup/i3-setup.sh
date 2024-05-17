41929d7c83bf4460b258ca696559baa403e4588f
craig91 <craigdejean1@gmail.com>
2024-05-17 14:08:06 +0900

    added firaCode font install function

--- arch-i3-setup/i3-setup.sh
+++ arch-i3-setup/i3-setup.sh
@@ -5,7 +5,8 @@
 
 #get from github Dot files, launch.sh
 
-ARCH_PACKAGES = ("vim" "polybar" "neofetch" "feh" "gnome-font-viewer" "picom" "kitty" "chromium" "lxappearance" "nodejs" "brightnessctl" "gparted" "thunar" "firefox" "discord")
+ARCH_PACKAGES = ("vim" "polybar" "neofetch" "feh" "gnome-font-viewer" "picom" "kitty" "chromium" "lxappearance" "nodejs" "brightnessctl" "gparted" "thunar" "firefox" "discord" "unzip")
+BINARY_LIST = com -23 <(pacman -Qqe | sort) <(expac -l '\n' '$E' base | sort) | grep $prog
 
 setupArch() {
     sudo pacman -Syu
@@ -21,4 +22,21 @@ setupArch() {
         fi
     done 
 }
+
+install_Fira_Code() {
+    if [ ! -d /$HOME/.local/share/fonts/nerd-fonts ]; then
+        mkdir -p /$HOME/.local/share/fonts/nerd-fonts
+    fi
+    echo "[+] Downloading Fonts [+]"
+    wget -P ~/.local/share/fonts/nerd-fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip
+    cd /$HOME/.local/share/fonts/nerd-fonts
+    pwd
+    unzip FiraCode.zip
+    echo "[+] Confirm if FiraCode is on this list of fonts in your system [+]"
+    fc-list | grep FiraCode
+}
+
+# Bumblebee_status() {
+#     if [bumblebee-status -eq ${BINARY_LIST}; then
+# }
 
