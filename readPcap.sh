ef0f547e56fe4fe75571b24c874c34142b601334
craig91 <craigdejean1@gmail.com>
2024-11-01 13:46:44 -0400

    Added source and destination port retrieval functions

--- readPcap.sh
+++ readPcap.sh
@@ -20,7 +20,25 @@ function getDstIP() {
     done
 }
 
-OPTFLAGS=":s:d:"
+function getSrcPort() {
+    local pcap_file=$1
+    printf "%-20s %-10s\n" "Source Port" "Count" 
+    printf "%-20s %-10s\n" "-----------" "-----"
+    tcpdump -tttt -r $pcap_file -n tcp 2>&1 | cut -d " " -f 4 | cut -d "." -f 5 | sort | uniq -c | sort -nr | while read Count ports; do
+        printf "%-20s %-10d\n" "$ports" "$Count"
+    done
+}
+
+function getDstPort() {
+    local pcap_file=$1
+    printf "%-20s %-10s\n" "Destination Port" "Count" 
+    printf "%-20s %-10s\n" "----------------" "-----"
+    tcpdump -tttt -r $pcap_file -n tcp 2>&1 | cut -d " " -f 6 | cut -d "." -f 5 | sort | uniq -c | sort -nr | while read Count ports; do
+        printf "%-20s %-10d\n" "$ports" "$Count"
+    done
+}
+
+OPTFLAGS=":s:d:r:p:"
 
 while getopts "$OPTFLAGS" flag; do
     case "$flag" in
@@ -29,6 +47,12 @@ while getopts "$OPTFLAGS" flag; do
             ;;
         d)
             getDstIP "$OPTARG"
+            ;;
+        r)
+            getSrcPort "$OPTARG"
+            ;;
+        p)
+            getDstPort "$OPTARG"
             ;;
         :)  
             echo "Option -${OPTARG} requires a pcap file as an argument."
